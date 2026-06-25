1da3a897895f9443989a2f12ed7e3398222a6a9f
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 22:17:36 +0400

    Update entity.h

--- entity.h
+++ entity.h
@@ -1,18 +1,25 @@
 #ifndef ENTITY_H
 #define ENTITY_H
-#include "utils.h"  // Все равно включает, но защищено
+#include "game_logic.h"
 
-typedef struct {
-    float x, y, speed;
-} Player;
+typedef struct { float x, y, speed; } Player;
 
 static inline void entity_update_player(Player* p, float dx, float dy) {
-    p->x += dx * p->speed;
-    p->y += dy * p->speed;
-    if(p->x < -1000) p->x = -1000; 
-    if(p->x > 1000) p->x = 1000;
-    if(p->y < -1000) p->y = -1000; 
-    if(p->y > 1000) p->y = 1000;
-}
+    float next_x = p->x + dx * p->speed;
+    float next_y = p->y + dy * p->speed;
 
+    // Коллизия по X
+    int mx = (int)(next_x / TILE_SIZE);
+    int my = (int)(p->y / TILE_SIZE);
+    if(mx >= 0 && mx < MAP_W && my >= 0 && my < MAP_H && WORLD_MAP[my][mx] == 0) {
+        p->x = next_x;
+    }
+    
+    // Коллизия по Y
+    mx = (int)(p->x / TILE_SIZE);
+    my = (int)(next_y / TILE_SIZE);
+    if(mx >= 0 && mx < MAP_W && my >= 0 && my < MAP_H && WORLD_MAP[my][mx] == 0) {
+        p->y = next_y;
+    }
+}
 #endif
