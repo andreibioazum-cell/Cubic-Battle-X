fca4bcd73bff168376a2fa5d6fa9f7b6a52700cb
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 20:35:13 +0400

    Delete shaders.h

--- shaders.h
+++
@@ -1,5 +0,0 @@
-#ifndef SHADERS_H
-#define SHADERS_H
-static const char* VS = "uniform mat4 m; attribute vec4 p; void main(){ gl_Position = m * p; }";
-static const char* FS = "precision mediump float; uniform vec4 c; void main(){ gl_FragColor = c; }";
-#endif
