1015ab7c68404db1f5ec7ea36c39466fac00deba
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 23:48:37 +0400

    Update entity.h

--- entity.h
+++ entity.h
@@ -7,6 +7,6 @@ typedef struct { float x, y, speed, angle; } Player;
 static inline void entity_update_player(Player* p, float dx, float dy) {
     p->x += dx * p->speed;
     p->y += dy * p->speed;
-    p->angle = atan2f(dy, dx); // Поворачиваем туда, куда идем
+    p->angle = atan2f(dy, dx);
 }
 #endif
