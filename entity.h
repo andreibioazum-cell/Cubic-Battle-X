2ee45905870eded5d5eb227a7bd51c19eb2060c2
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 23:29:06 +0400

    Update entity.h

--- entity.h
+++ entity.h
@@ -1,13 +1,12 @@
 #ifndef ENTITY_H
 #define ENTITY_H
+#include <math.h>
 
-typedef struct { float x, y, speed; } Player;
+typedef struct { float x, y, speed, angle; } Player;
 
 static inline void entity_update_player(Player* p, float dx, float dy) {
     p->x += dx * p->speed;
     p->y += dy * p->speed;
-    // Границы мира (необязательно, но для порядка)
-    if(p->x < -2000) p->x = -2000; if(p->x > 2000) p->x = 2000;
-    if(p->y < -2000) p->y = -2000; if(p->y > 2000) p->y = 2000;
+    p->angle = atan2f(dy, dx); // Поворачиваем туда, куда идем
 }
 #endif
