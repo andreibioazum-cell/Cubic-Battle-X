cf75388edec59e1d59eaaed18fd3a1bad7b975e8
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 22:21:04 +0400

    Update entity.h

--- entity.h
+++ entity.h
@@ -1,25 +1,13 @@
 #ifndef ENTITY_H
 #define ENTITY_H
-#include "game_logic.h"
 
 typedef struct { float x, y, speed; } Player;
 
 static inline void entity_update_player(Player* p, float dx, float dy) {
-    float next_x = p->x + dx * p->speed;
-    float next_y = p->y + dy * p->speed;
-
-    // Коллизия по X
-    int mx = (int)(next_x / TILE_SIZE);
-    int my = (int)(p->y / TILE_SIZE);
-    if(mx >= 0 && mx < MAP_W && my >= 0 && my < MAP_H && WORLD_MAP[my][mx] == 0) {
-        p->x = next_x;
-    }
-    
-    // Коллизия по Y
-    mx = (int)(p->x / TILE_SIZE);
-    my = (int)(next_y / TILE_SIZE);
-    if(mx >= 0 && mx < MAP_W && my >= 0 && my < MAP_H && WORLD_MAP[my][mx] == 0) {
-        p->y = next_y;
-    }
+    p->x += dx * p->speed;
+    p->y += dy * p->speed;
+    // Границы мира (необязательно, но для порядка)
+    if(p->x < -2000) p->x = -2000; if(p->x > 2000) p->x = 2000;
+    if(p->y < -2000) p->y = -2000; if(p->y > 2000) p->y = 2000;
 }
 #endif
