aa2019316cc1d887e89ad3d2722302cf73668b22
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 23:58:28 +0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,8 +1,12 @@
-#ifndef UI_H
+ #ifndef UI_H
 #define UI_H
 #include "utils.h"
 
-typedef struct { float sx, sy, cx, cy; int active; } Joystick;
+typedef struct { 
+    float sx, sy, cx, cy; 
+    int active; 
+    int joy_pid; // ВОТ ЭТОЙ СТРОЧКИ НЕ ХВАТАЛО
+} Joystick;
 
 static inline void ui_draw_circle(GLint mvp_loc, float x, float y, float r, float thick, mat4 m) {
     const int segs = 64;
@@ -13,7 +17,7 @@ static inline void ui_draw_circle(GLint mvp_loc, float x, float y, float r, floa
 
     if (thick > 0) {
         for(int i=0; i<=segs; i++) {
-            float a = i * 2.0f * 3.14159f / segs;
+            float a = i * 2.0f * 3.14159f / (float)segs;
             v[i*4]=x+cosf(a)*r; v[i*4+1]=y+sinf(a)*r;
             v[i*4+2]=x+cosf(a)*(r-thick); v[i*4+3]=y+sinf(a)*(r-thick);
         }
@@ -22,7 +26,7 @@ static inline void ui_draw_circle(GLint mvp_loc, float x, float y, float r, floa
     } else {
         v[0]=x; v[1]=y;
         for(int i=0; i<=segs; i++) {
-            float a = i * 2.0f * 3.14159f / segs;
+            float a = i * 2.0f * 3.14159f / (float)segs;
             v[(i+1)*2]=x+cosf(a)*r; v[(i+1)*2+1]=y+sinf(a)*r;
         }
         glUniformMatrix4fv(mvp_loc, 1, 0, m.m);
