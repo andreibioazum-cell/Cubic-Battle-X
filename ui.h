11a3d078457ecc224e74696b6486aebdbb28084a
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 23:48:13 +0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -5,28 +5,28 @@
 typedef struct { float sx, sy, cx, cy; int active; } Joystick;
 
 static inline void ui_draw_circle(GLint mvp_loc, float x, float y, float r, float thick, mat4 m) {
-    const int segs = 128; // Больше сегментов = плавнее круг
-    float v[(segs + 1) * 4]; 
+    const int segs = 64;
+    float v[(segs + 1) * 4];
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, v);
-    glDisableVertexAttribArray(1); 
+    glDisableVertexAttribArray(1);
 
     if (thick > 0) {
         for(int i=0; i<=segs; i++) {
-            float a = i * 2.0f * 3.14159f / (float)segs;
+            float a = i * 2.0f * 3.14159f / segs;
             v[i*4]=x+cosf(a)*r; v[i*4+1]=y+sinf(a)*r;
             v[i*4+2]=x+cosf(a)*(r-thick); v[i*4+3]=y+sinf(a)*(r-thick);
         }
         glUniformMatrix4fv(mvp_loc, 1, 0, m.m);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, (segs + 1) * 2);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, (segs+1)*2);
     } else {
         v[0]=x; v[1]=y;
         for(int i=0; i<=segs; i++) {
-            float a = i * 2.0f * 3.14159f / (float)segs;
+            float a = i * 2.0f * 3.14159f / segs;
             v[(i+1)*2]=x+cosf(a)*r; v[(i+1)*2+1]=y+sinf(a)*r;
         }
         glUniformMatrix4fv(mvp_loc, 1, 0, m.m);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, segs + 2);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, segs+2);
     }
 }
 #endif
