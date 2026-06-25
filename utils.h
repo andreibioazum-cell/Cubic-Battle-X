51bd3045376a1451e2eb3a434fad795ea882dbe0
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 23:28:32 +0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -17,6 +17,27 @@ static inline void mat4_translate(mat4* m, float x, float y) {
     m->m[12] += m->m[0] * x; m->m[13] += m->m[5] * y;
 }
 
+// Новая функция для рисования повернутого квадрата
+static inline void draw_quad_ext(GLint mvp_loc, float x, float y, float w, float h, float tx, float ty, float angle, mat4 view) {
+    mat4 model = view;
+    mat4_translate(&model, x, y);
+    
+    // Вращение (по Z)
+    float s = sinf(angle), c = cosf(angle);
+    float m0 = model.m[0], m5 = model.m[5];
+    model.m[0] = c * m0;  model.m[1] = s * m5;
+    model.m[4] = -s * m0; model.m[5] = c * m5;
+
+    float v[] = { -w/2,-h/2, 0,0, -w/2,h/2, 0,ty, w/2,-h/2, tx,0, w/2,h/2, tx,ty };
+    glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, model.m);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 16, v);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 16, &v[2]);
+    glEnableVertexAttribArray(1);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+// Обычный квадрат для пола
 static inline void draw_quad(GLint mvp_loc, float x, float y, float w, float h, float tx, float ty, mat4 view) {
     float v[] = { x,y, 0,0, x,y+h, 0,ty, x+w,y, tx,0, x+w,y+h, tx,ty };
     glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, view.m);
