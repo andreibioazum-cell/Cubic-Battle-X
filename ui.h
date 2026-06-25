819c915333e290399cc6e6d8da7499e6e5f73666
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-25 22:16:45 +0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,50 +1,32 @@
 #ifndef UI_H
 #define UI_H
-#include "utils.h"
+#include <GLES2/gl2.h>
+#include "math_util.h"
 
-typedef struct {
-    float sx, sy, cx, cy;
-    int active;
-} Joystick;
+typedef struct { float sx, sy, cx, cy; int active; } Joystick;
 
-static inline void ui_draw_circle(GLint mvp_loc, GLint col_loc, 
-                                  float x, float y, float r, 
-                                  float thick, mat4 m) {
-    #define SEGMENTS 100
-    float v[(SEGMENTS+1)*4];
-    
-    glUniformMatrix4fv(mvp_loc, 1, 0, m.m);
-    
+static inline void ui_draw_circle(GLint mvp_loc, float x, float y, float r, float thick, mat4 m) {
+    float v[404];
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, v);
+    glDisableVertexAttribArray(1); // Текстуры в UI не нужны
+
     if (thick > 0) {
-        // Рисуем обводку (кольцо)
-        for(int i=0; i<=SEGMENTS; i++) {
-            float a = i * 2.0f * M_PI / SEGMENTS;
-            float cos_a = cosf(a);
-            float sin_a = sinf(a);
-            // Внутренняя точка
-            v[i*4] = x + cos_a * (r - thick);
-            v[i*4+1] = y + sin_a * (r - thick);
-            // Внешняя точка
-            v[i*4+2] = x + cos_a * r;
-            v[i*4+3] = y + sin_a * r;
+        for(int i=0; i<=100; i++) {
+            float a = i * 2.0f * 3.14159f / 100.0f;
+            v[i*4] = x + cosf(a)*r; v[i*4+1] = y + sinf(a)*r;
+            v[i*4+2] = x + cosf(a)*(r-thick); v[i*4+3] = y + sinf(a)*(r-thick);
         }
-        glVertexAttribPointer(0, 2, GL_FLOAT, 0, 4*4, v);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 2, GL_FLOAT, 0, 4*4, &v[2]);
-        glEnableVertexAttribArray(1);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, (SEGMENTS+1)*2);
+        glUniformMatrix4fv(mvp_loc, 1, 0, m.m);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 202);
     } else {
-        // Рисуем залитый круг
-        for(int i=0; i<=SEGMENTS; i++) {
-            float a = i * 2.0f * M_PI / SEGMENTS;
-            v[i*2] = x + cosf(a) * r;
-            v[i*2+1] = y + sinf(a) * r;
+        v[0] = x; v[1] = y;
+        for(int i=0; i<=100; i++) {
+            float a = i * 2.0f * 3.14159f / 100.0f;
+            v[(i+1)*2] = x + cosf(a)*r; v[(i+1)*2+1] = y + sinf(a)*r;
         }
-        glVertexAttribPointer(0, 2, GL_FLOAT, 0, 2*4, v);
-        glEnableVertexAttribArray(0);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, SEGMENTS+1);
+        glUniformMatrix4fv(mvp_loc, 1, 0, m.m);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 102);
     }
-    #undef SEGMENTS
 }
-
 #endif
