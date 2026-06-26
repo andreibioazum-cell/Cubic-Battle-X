ee1badb532b45f2683bdc40240f3f043344304a6
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-26 20:16:45 +0400

    Update shaders.h

--- shaders.h
+++ shaders.h
@@ -1,5 +1,30 @@
 #ifndef SHADERS_H
 #define SHADERS_H
-static const char* VS = "uniform mat4 m; attribute vec4 p; attribute vec2 uv; varying vec2 v_uv; void main(){ gl_Position = m * p; v_uv = uv; }";
-static const char* FS = "precision mediump float; uniform vec4 c; uniform sampler2D tex; uniform int use_tex; varying vec2 v_uv; void main(){ if(use_tex==1){ gl_FragColor = texture2D(tex, v_uv); } else { gl_FragColor = c; } }";
+
+static const char* VS = 
+    "uniform mat4 m;"
+    "attribute vec4 p;"
+    "attribute vec2 uv;"
+    "varying vec2 v_uv;"
+    "void main(){"
+    "  gl_Position = m * p;"
+    "  v_uv = uv;"
+    "}";
+
+static const char* FS = 
+    "precision mediump float;"
+    "uniform vec4 c;"
+    "uniform sampler2D tex;"
+    "uniform int use_tex;"
+    "varying vec2 v_uv;"
+    "void main(){"
+    "  if(use_tex == 1) {" // Режим для обычных текстур (игрок, пол)
+    "    gl_FragColor = texture2D(tex, v_uv);"
+    "  } else if (use_tex == 2) {" // РЕЖИМ ДЛЯ ШРИФТА (только прозрачность)
+    "    gl_FragColor = vec4(c.rgb, texture2D(tex, v_uv).a);"
+    "  } else {" // Режим для простых фигур (цвет)
+    "    gl_FragColor = c;"
+    "  }"
+    "}";
+
 #endif
