143cc73cff2f65c935fc219a2af43161d96a9d12
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-26 14:36:26 +0400

    Update game_logic.h

--- game_logic.h
+++ game_logic.h
@@ -1,19 +1,22 @@
 #ifndef GAME_LOGIC_H
 #define GAME_LOGIC_H
 
-#define MAP_W 16
-#define MAP_H 9
-#define TILE_SIZE 100.0f
+#define MAP_W 32
+#define MAP_H 20
+#define TILE_SIZE 120.0f // Увеличим размер тайла
 
-static char WORLD_MAP[MAP_H][MAP_W] = {
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-    {1,0,1,1,1,0,1,1,1,0,1,1,1,1,0,1},
-    {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1},
-    {1,0,0,0,0,1,1,0,1,1,0,0,0,0,0,1},
-    {1,0,1,0,0,1,1,0,1,1,0,0,0,1,0,1},
-    {1,0,1,1,1,0,0,0,0,0,1,1,1,1,0,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
+static char WORLD_MAP[MAP_H][MAP_W] = {0}; // Инициализируем нулями
+
+// Функция для создания границ
+static inline void create_map_borders() {
+    for(int y = 0; y < MAP_H; y++) {
+        for(int x = 0; x < MAP_W; x++) {
+            if(y == 0 || y == MAP_H - 1 || x == 0 || x == MAP_W - 1) {
+                WORLD_MAP[y][x] = 1; // Стена
+            } else {
+                WORLD_MAP[y][x] = 0; // Пусто
+            }
+        }
+    }
+}
 #endif
