16aa68354fc32149645e6186ff7aa177d2bee6a2
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-26 10:19:51 +0400

    Update lobby.h

--- lobby.h
+++ lobby.h
@@ -1,24 +1,19 @@
 #ifndef LOBBY_H
 #define LOBBY_H
-#include "utils.h"
 #include "font.h"
 
 typedef enum { STATE_LOBBY, STATE_GAME } GameState;
 
 static inline void draw_lobby(GLint mvp_loc, GLint col_loc, int w, int h, mat4 view) {
-    glUniform4f(col_loc, 0.1f, 0.1f, 0.1f, 1.0f);
-    draw_quad(mvp_loc, 0, 0, w, h, 0, 0, view); // Фон
-
-    // Стильная кнопка (Синяя с обводкой)
-    glUniform4f(col_loc, 0.2f, 0.4f, 0.8f, 1.0f);
-    draw_quad(mvp_loc, w/2 - 160, h/2 - 50, 320, 100, 0, 0, view);
-    
-    // "Текст" (цифра 0 как символ старта для примера)
+    glUniform4f(col_loc, 0.05f, 0.05f, 0.1f, 1.0f);
+    draw_quad(mvp_loc, 0, 0, w, h, 0, 0, view);
+    glUniform4f(col_loc, 0.2f, 0.5f, 1.0f, 1.0f); // Кнопка синяя
+    draw_quad(mvp_loc, w/2 - 150, h/2 - 50, 300, 100, 0, 0, view);
     glUniform4f(col_loc, 1, 1, 1, 1);
-    draw_digit(mvp_loc, col_loc, 0, w/2 - 20, h/2 - 20, 5, view);
+    draw_digit(mvp_loc, 0, w/2 - 20, h/2 - 20, 5, view); // Рисуем "0" как символ старта
 }
 
-static inline int lobby_is_clicked(float x, float y, int w, int h) {
-    return (x > w/2 - 160 && x < w/2 + 160 && y > h/2 - 50 && y < h/2 + 50);
+static inline int lobby_check(float x, float y, int w, int h) {
+    return (x > w/2 - 150 && x < w/2 + 150 && y > h/2 - 50 && y < h/2 + 50);
 }
 #endif
