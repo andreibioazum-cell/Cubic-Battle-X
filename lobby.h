1f74fc43043c3e403566b9cf0240c745568c9a2c
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-27 11:15:56 +0400

    Update lobby.h

--- lobby.h
+++ lobby.h
@@ -8,21 +8,13 @@ typedef enum { STATE_LOBBY, STATE_GAME } GameState;
 static inline void draw_lobby(GLint mvp_loc, GLint col_loc, GLint use_tex_loc, GLuint p_loc, GLuint uv_loc, int w, int h, Font* font, mat4 view) {
     float btn_w = 300, btn_h = 100;
     float btn_x = w/2.0f - btn_w/2.0f, btn_y = h/2.0f - btn_h/2.0f;
-
-    // Фон
     glUniform1i(use_tex_loc, 0);
     glUniform4f(col_loc, 0.05f, 0.05f, 0.1f, 1.0f);
     draw_quad(mvp_loc, p_loc, uv_loc, 0, 0, w, h, 0, 0, view);
-
-    // Обводка
     glUniform4f(col_loc, 0, 0, 0, 1.0f);
     draw_quad(mvp_loc, p_loc, uv_loc, btn_x - 4, btn_y - 4, btn_w + 8, btn_h + 8, 0, 0, view);
-
-    // Кнопка
     glUniform4f(col_loc, 0.5f, 0.2f, 0.8f, 1.0f);
     draw_quad(mvp_loc, p_loc, uv_loc, btn_x, btn_y, btn_w, btn_h, 0, 0, view);
-    
-    // Текст
     glUniform1i(use_tex_loc, 2);
     glUniform4f(col_loc, 1.0f, 1.0f, 1.0f, 1.0f);
     glBindTexture(GL_TEXTURE_2D, font->tex_id);
