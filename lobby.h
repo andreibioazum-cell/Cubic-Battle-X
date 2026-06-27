5832013c1dba41bca7a4d788c41a3e54f4135466
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-27 11:41:14 +0400

    Update lobby.h

--- lobby.h
+++ lobby.h
@@ -9,17 +9,25 @@ static inline void draw_lobby(GLint mvp_loc, GLint col_loc, GLint use_tex_loc, G
     glUniform1i(use_tex_loc, 0);
     glUniform4f(col_loc, 0.05f, 0.05f, 0.1f, 1.0f);
     draw_quad(mvp_loc, p_loc, uv_loc, 0, 0, (float)w, (float)h, 0, 0, view);
-    float btn_w = 400, btn_h = 120, bx = w/2.0f - btn_w/2.0f, by = h/2.0f - btn_h/2.0f;
-    glUniform4f(col_loc, 0, 0, 0, 1);
-    draw_quad(mvp_loc, p_loc, uv_loc, bx-5, by-5, btn_w+10, btn_h+10, 0, 0, view);
+
+    float btn_w = 400, btn_h = 120;
+    float bx = (float)w/2.0f - btn_w/2.0f;
+    float by = (float)h/2.0f - btn_h/2.0f;
+
+    glUniform4f(col_loc, 0, 0, 0, 1.0f);
+    draw_quad(mvp_loc, p_loc, uv_loc, bx - 5, by - 5, btn_w + 10, btn_h + 10, 0, 0, view);
     glUniform4f(col_loc, 0.5f, 0.0f, 0.7f, 1.0f);
     draw_quad(mvp_loc, p_loc, uv_loc, bx, by, btn_w, btn_h, 0, 0, view);
-    glUniform1i(use_tex_loc, 2); glUniform4f(col_loc, 1, 1, 1, 1);
-    font_draw_text(p_loc, uv_loc, font, "PLAY", w/2.0f - 60, h/2.0f + 20);
+    
+    glUniform1i(use_tex_loc, 2);
+    glUniform4f(col_loc, 1.0f, 1.0f, 1.0f, 1.0f);
+    font_draw_text(p_loc, uv_loc, font, "PLAY", (float)w/2.0f - 60, (float)h/2.0f + 20);
 }
 
 static inline int lobby_is_clicked(float x, float y, int w, int h) {
-    float btn_w = 400, btn_h = 120, bx = w/2.0f - btn_w/2.0f, by = h/2.0f - btn_h/2.0f;
+    float btn_w = 400, btn_h = 120;
+    float bx = (float)w/2.0f - btn_w/2.0f;
+    float by = (float)h/2.0f - btn_h/2.0f;
     return (x > bx && x < bx + btn_w && y > by && y < by + btn_h);
 }
 #endif
